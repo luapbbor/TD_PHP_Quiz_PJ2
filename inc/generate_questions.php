b2a56016cec381d4bd47b9c0729a60f8b3e66663
luapbbor <luapbbor@gmail.com>
2020-09-18 09:53:53 +1000

    Adjustments to possible answers

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -11,26 +11,40 @@ $questions = array();
 for ($i = 0; $i<25; $i++) {
 
 // variables for random numbers
- $ranNum1 = rand(8,75);
+ $ranNum1 = rand(8,50);
  $ranNum2 = rand(5,25);
- $ran10 = rand(1,10);
+ $ran10 = rand(-10,10);
+ $ran9 = rand(-9,9);
 
  // Calculate correct answer
 $correctAnswer = $ranNum1 + $ranNum2;  
 
-// Get incorrect answers within 10 numbers either way of correct answer
-// Make sure it is a unique answer
+// incorrect answers = correct answer plus a random number in a range
+$firstIncorrect = $correctAnswer + $ran10;
+$secondIncorrect = $correctAnswer + $ran9;
 
- $firstIncorrect = $correctAnswer + $ran10;
- $secondIncorrect = $correctAnswer - $ran10;
-
- // Add keys / values to the array at each iteration of the loop
+ // Add the 2 random numbers that add up 
  $questions[$i]["leftAdder"] = $ranNum1;
  $questions[$i]["rightAdder"] = $ranNum2;
- $questions[$i]["correctAnswer"] = $correctAnswer;
- $questions[$i]["firstIncorrectAnswer"] = $firstIncorrect;
- $questions[$i]["secondIncorrectAnswer"] = $secondIncorrect;
 
+ // put the correct answer in the array
+ $questions[$i]["correctAnswer"] = $correctAnswer;
+
+  
+//  $questions[$i]["secondIncorrectAnswer"] = $secondIncorrect;   
+
+// Get incorrect answers within 10 numbers either way of correct answer
+// The below  Make sure it is a unique answer
+if ($firstIncorrect == $correctAnswer) {
+$firstIncorrect += $ran10;
+}
+
+if ($secondIncorrect == $correctAnswer || $secondIncorrect == $firstIncorrect) {
+$secondIncorrect += $ran9;
+}
+
+$questions[$i]["firstIncorrectAnswer"] = $firstIncorrect;    
+$questions[$i]["secondIncorrectAnswer"] = $secondIncorrect;    
 }
 
 // After the loop, return the array
